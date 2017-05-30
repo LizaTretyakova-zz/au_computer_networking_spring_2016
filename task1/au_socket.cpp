36a70c0c85f25d9a05ef5269dc7bffdb7ab55dea
LizaTretyakova <elizabet.tretyakova@gmail.com>
2017-05-30 19:09:15 +0300

    sending messages

--- task1/au_socket.cpp
+++ task1/au_socket.cpp
@@ -19,20 +19,12 @@ using std::cerr;
 using std::endl;
 using std::string;
 
-static void check_socket_set(int sockfd) {
-    cerr << "check socket with fd " << sockfd << endl;
-    if (sockfd < 0) {
-        throw std::runtime_error("Socket was not set properly, revert\n");
-    }
-}
-
 au_socket::au_socket(hostname a, au_stream_port cp, au_stream_port sp):
-    addr(a), cport(cp), sport(sp) {
-    if ((sockfd = socket(PF_INET, SOCK_RAW, IPPROTO_AU)) == -1) {
+    addr(a), local_port(cp), remote_port(sp) {
+    if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_AU)) == -1) {
         perror("AU client: error creating socket");
         throw std::runtime_error("socket() call failed");
     }
-    fprintf(stderr, "%p\n", addr);
     cerr << "created socket with fd " << sockfd << endl;
     memset(buffer, 0, AU_BUF_SIZE);
 }
@@ -44,7 +36,7 @@ au_socket::au_socket(int new_fd, hostname a, au_stream_port cp, au_stream_port s
 
 au_server_socket::au_server_socket(hostname a, au_stream_port port_number):
     addr(a), port(port_number) {
-    if ((sockfd = socket(PF_INET, SOCK_RAW, IPPROTO_AU)) == -1) {
+    if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_AU)) == -1) {
         perror("AU server: error creating socket");
         throw std::runtime_error("socket() call failed");
     }
@@ -52,24 +44,12 @@ au_server_socket::au_server_socket(hostname a, au_stream_port port_number):
 }
 
 void au_socket::send(const void* buf, size_t size) {
-    check_socket_set(sockfd);
+    check_socket_set();
 
     // Address resolution stuff
     struct sockaddr_in sin;
     memset (&sin, 0, sizeof (struct sockaddr_in));
-    int res = inet_pton(PF_INET, addr, &(sin.sin_addr));
-    if(res == -1) {
-        perror("send: error while converting IP addr");
-        throw std::runtime_error("AU Socket send: error while converting IP addr");
-    }
-
-    char tmp[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(sin.sin_addr), tmp, INET_ADDRSTRLEN);
-    cerr << "send to sin.sin_addr.s_addr: " << string(tmp) << endl;
-
-    sin.sin_port = htons(sport);
-    cerr << "send to sin.sin_port: " << ntohs(sin.sin_port) << endl;
-    sin.sin_family = PF_INET;
+    get_remote_sockaddr(&sin);
 
     // Filling in the packet
     // (only one packet for now)
@@ -84,57 +64,27 @@ void au_socket::send(const void* buf, size_t size) {
      */
     // TODO
 
-    // memset buffer to all zeroes
-    memset(buffer, 0, AU_BUF_SIZE);
-    // omit writing ip header
-    // write tcphdr
-    struct tcphdr* tcph = (struct tcphdr*)(buffer);
-    tcph->th_sum = 0;
-    // write data
-    char* data = buffer + sizeof(tcphdr);
-    memcpy(data, buf, std::min(size, AU_BUF_CAPACITY));
-    cerr << "wrote data to buffer:\n" << string(data) << endl;
-    data[AU_BUF_SIZE - 1] = 0;
+    // form packet
+    form_packet(buf, size);
 
+    // send
     int sent = 0;
-    size_t data_len = sizeof (struct tcphdr) + strlen(data);
-    cerr << data_len << endl;
+    size_t hdr_len = sizeof(struct tcphdr);
+    size_t data_len = hdr_len + strlen(buffer + hdr_len);
     sent = ::sendto(sockfd, buffer, data_len, 0,
                     (struct sockaddr *)(&sin), sizeof(sin));
     if(sent == -1) {
         perror("AU Socket: error while sending");
         throw std::runtime_error("AU Socket: error while sending");
-    } else {
-        cerr << "sent " << sent << " bytes of data_len " << data_len << endl;
     }
-}
-
-bool au_socket::is_ours() {
-    struct iphdr *iph = (struct iphdr *)buffer;
-    struct tcphdr *tcph = (struct tcphdr*)(buffer + sizeof(struct ip));
-    struct sockaddr_in source;
-    memset(&source, 0, sizeof(source));
-    source.sin_addr.s_addr = iph->saddr;
-
-    cerr << "meeow";
-    cerr << (const char*)addr;
-    string caddr = string(addr);
-    char tmp[INET_ADDRSTRLEN];
-    string saddr = string(inet_ntop(PF_INET, &(source.sin_addr), tmp, INET_ADDRSTRLEN));
-    cerr << caddr << " " << saddr << endl;
-
-    return tcph->th_sport == sport
-            && tcph->th_dport == cport
-            && strcmp(addr, inet_ntoa(source.sin_addr));
+    cerr << "sent " << sent << " bytes of data_len " << data_len << endl;
 }
 
 void au_socket::recv(void *buf, size_t size) {
-    check_socket_set(sockfd);
+    check_socket_set();
 
     struct sockaddr saddr;
     socklen_t saddr_size = sizeof(saddr);
-
-    fprintf(stderr, "%p\n", addr);
 
     /*
      * while (hasn't yet received the finishing message) { */
@@ -145,7 +95,6 @@ void au_socket::recv(void *buf, size_t size) {
             perror("AU Socket: error while receiving");
             throw std::runtime_error("AU Socket: error while receiving");
         }
-        cerr << "meow " << endl;
         cerr << "meow " << is_ours() << endl;
     } while(!is_ours());
     size_t iphdrlen = sizeof(struct ip);
@@ -153,38 +102,15 @@ void au_socket::recv(void *buf, size_t size) {
     memcpy(buf, buffer + iphdrlen + tcphdrlen, std::min((size_t)(res - iphdrlen - tcphdrlen), size));
 
     cerr << "received " << res << " bytes of data:" << endl;
-    char* data = (char*)buf;
-    for(int i = 0; (size_t)i < std::min((size_t)(res - iphdrlen - tcphdrlen), size); ++i) {
-        if(data[i] >= 32) {
-            cerr << data[i];
-        } else {
-            cerr << "[" << (int)data[i] << "] ";
-        }
-    }
-    cerr << endl;
     /* }
      */
 }
 
-bool au_socket::handshake(struct addrinfo* servinfo) {
-    // the dummy one for now
-    (void)servinfo;
-    return true;
-}
-
 void au_client_socket::connect() {
-    check_socket_set(sockfd);
+    check_socket_set();
 
 }
 
 stream_socket* au_server_socket::accept_one_client() {
     return NULL;
-}
-
-void fill_tcp_header(struct tcphdr *tcph, au_stream_port src_port, au_stream_port dst_port, unsigned int seq) {
-    memset(tcph, 0, sizeof(struct tcphdr));
-
-    tcph->th_sport = htons(src_port);
-    tcph->th_dport = htons(dst_port);
-    tcph->th_seq = seq;
 }
--- task1/au_socket.h
+++ task1/au_socket.h
@@ -10,16 +10,19 @@
 #include <netinet/tcp.h>      //Provides declarations for tcp header
 #include <stdexcept>
 #include <stdio.h>
+#include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 using std::cout;
 using std::cerr;
+using std::string;
 
 typedef unsigned short au_stream_port;
 
 const hostname DEFAULT_AU_ADDR = "127.0.0.1";
+const hostname AU_LOCAL_ADDR = "127.0.0.1";
 const au_stream_port DEFAULT_AU_CLIENT_PORT = 40001;
 const au_stream_port DEFAULT_AU_SERVER_PORT = 301;
 const unsigned char DEFAULT_AU_OFFSET = 5;
@@ -31,13 +34,17 @@ struct au_socket: virtual stream_socket {
 
 protected:
     hostname addr;
-    au_stream_port cport;
-    au_stream_port sport;
+    au_stream_port local_port;
+    au_stream_port remote_port;
     int sockfd;
     char buffer[AU_BUF_SIZE];
 
     bool is_ours();
     bool handshake(struct addrinfo* servinfo);
+    void get_local_sockaddr(struct sockaddr_in* dst);
+    void get_remote_sockaddr(struct sockaddr_in* dst);
+    void form_packet(const void* buf, size_t size);
+    void check_socket_set();
 
 public:
     au_socket(hostname a = DEFAULT_AU_ADDR,
